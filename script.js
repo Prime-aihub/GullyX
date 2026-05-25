e928407c2b92acda4775943ac7e50ce7adc6ac1a
Prime-aihub <primeaih@gmail.com>
2026-05-25 16:29:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const themeToggle = document.getElementById('themeToggle');
+
+themeToggle.addEventListener('click', () => {
+
+  document.body.classList.toggle('light');
+
+  if(document.body.classList.contains('light')) {
+    themeToggle.innerHTML = '🌙';
+  } else {
+    themeToggle.innerHTML = '⚽';
+  }
+
+});
+
+console.log('GullyX Loaded Successfully')
